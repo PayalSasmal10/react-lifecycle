adbe31c427425f742db8ef2799bf1a3b8c3deec2
Payal Sasmal <sasmalpayal@gmail.com>
2023-05-10 12:00:27 +0530

    react lifecycle creation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      count : 0,
+    }
+  }
+
+  componentDidMount(){
+    console.log("componentDidMount : When component render first time");
+  }
+
+  increment(){
+    this.setState({count : this.state.count + 1});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.count}</h1>
+        {/* <button onClick={this.increment.bind(this)}>Click Me</button>  // this is one of the way to increase the state */}
+        <button onClick={() => {this.increment()}}>Click Me</button>
+      </div>
+    );
+  }
 }
 
 export default App;
